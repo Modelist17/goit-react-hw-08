3e7a9b6ec2ff7dd374cdc58b0b17892566066dfc
Modelist17 <hunteruboat@gmail.com>
2024-06-24 00:40:48 +0200

    ahhhhhhhhhhhh

--- src/pages/RegistrationPage/RegistrationPage.jsx
+++ src/pages/RegistrationPage/RegistrationPage.jsx
@@ -1,6 +1,6 @@
 import PageTitle from "../../components/PageTitle/PageTitle";
 import RegistrationForm from "../../components/RegistrationForm/RegistrationForm";
-
+// ../../components/RegistrationForm/RegistrationForm
 export default function RegistrationPage() {
   return (
     <div>
